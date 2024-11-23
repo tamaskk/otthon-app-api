e858a02acd47abad72dc0704d5e57273736ede9a
Tamas Krisztian Kalman <kalman.tamaskrisztian@gmail.com>
2024-11-23 14:02:17 +0100

    fjx

--- pages/api/shopping-list.ts
+++ pages/api/shopping-list.ts
@@ -1,71 +1,88 @@
-import NextCors from 'nextjs-cors';
+import NextCors from "nextjs-cors";
 import { connectToDatabase } from "@/db/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ObjectId } from "mongodb";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        // Allow any origin and any method
-        await NextCors(req, res, {
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            origin: '*', // Allow all origins
-            optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-        });
+  try {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
 
-        const client = await connectToDatabase();
+    // Allow any origin and any method
+    await NextCors(req, res, {
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      origin: "*", // Allow all origins
+      optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
 
-        // Handle POST request
-        if (req.method === "POST") {
-            const { name, shop, quantity, comment, price } = req.body;
+    const client = await connectToDatabase();
 
-            if (!name || !shop || !quantity || !comment || !price) {
-                res.status(400).json({ success: false, message: "Missing field(s)" });
-                return;
-            }
+    // Handle POST request
+    if (req.method === "POST") {
+      const { name, shop, quantity, comment, price } = req.body;
 
-            const shoppingListCollection = client.db().collection("shopping-list");
-            await shoppingListCollection.insertOne({ name, shop, quantity, comment, price });
-            res.status(201).json({ success: true, data: "Sikeresen hozzáadva" });
+      if (!name || !shop || !quantity || !comment || !price) {
+        res.status(400).json({ success: false, message: "Missing field(s)" });
+        return;
+      }
 
-        // Handle GET request
-        } else if (req.method === "GET") {
-            const shoppingListCollection = client.db().collection("shopping-list");
-            const result = await shoppingListCollection.find({ isDone: false }).toArray();
-            res.status(200).json({ success: true, data: result });
+      const shoppingListCollection = client.db().collection("shopping-list");
+      await shoppingListCollection.insertOne({
+        name,
+        shop,
+        quantity,
+        comment,
+        price,
+      });
+      res.status(201).json({ success: true, data: "Sikeresen hozzáadva" });
 
-        // Handle DELETE request
-        } else if (req.method === "DELETE") {
-            const { id } = req.body;
-            if (!id) {
-                res.status(400).json({ success: false, message: "Missing field(s)" });
-                return;
-            }
+      // Handle GET request
+    } else if (req.method === "GET") {
+      const shoppingListCollection = client.db().collection("shopping-list");
+      const result = await shoppingListCollection
+        .find({ isDone: false })
+        .toArray();
+      res.status(200).json({ success: true, data: result });
 
-            const shoppingListCollection = client.db().collection("shopping-list");
-            await shoppingListCollection.deleteOne({ _id: new ObjectId(id) });
-            res.status(200).json({ success: true, data: "Sikeresen törölve" });
+      // Handle DELETE request
+    } else if (req.method === "DELETE") {
+      const { id } = req.body;
+      if (!id) {
+        res.status(400).json({ success: false, message: "Missing field(s)" });
+        return;
+      }
 
-        // Handle PATCH request
-        } else if (req.method === "PATCH") {
-            const { id } = req.body;
-            if (!id) {
-                res.status(400).json({ success: false, message: "Missing field(s)" });
-                return;
-            }
+      const shoppingListCollection = client.db().collection("shopping-list");
+      await shoppingListCollection.deleteOne({ _id: new ObjectId(id) });
+      res.status(200).json({ success: true, data: "Sikeresen törölve" });
 
-            const shoppingListCollection = client.db().collection("shopping-list");
-            await shoppingListCollection.updateOne({ _id: new ObjectId(id) }, { $set: { isDone: true } });
-            res.status(200).json({ success: true, data: "Sikeresen frissítve" });
+      // Handle PATCH request
+    } else if (req.method === "PATCH") {
+      const { id } = req.body;
+      if (!id) {
+        res.status(400).json({ success: false, message: "Missing field(s)" });
+        return;
+      }
 
-        // Handle invalid HTTP methods
-        } else {
-            res.status(405).json({ message: "Method not allowed" });
-        }
+      const shoppingListCollection = client.db().collection("shopping-list");
+      await shoppingListCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { isDone: true } }
+      );
+      res.status(200).json({ success: true, data: "Sikeresen frissítve" });
 
-    } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ success: false, message: "Server error" });
+      // Handle invalid HTTP methods
+    } else {
+      res.status(405).json({ message: "Method not allowed" });
     }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
 };
 
 export default handler;
