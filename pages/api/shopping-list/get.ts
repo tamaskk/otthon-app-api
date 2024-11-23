ac4efc5070aa528815344d6949c9b4c15ce58c8d
Tamas Krisztian Kalman <kalman.tamaskrisztian@gmail.com>
2024-11-23 15:17:51 +0100

    fix

--- pages/api/shopping-list/get.ts
+++ pages/api/shopping-list/get.ts
@@ -3,11 +3,13 @@ import { connectToDatabase } from "@/db/db";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ObjectId } from "mongodb";
 
-type Data = {};
+type Data = {
+    name: string;
+};
 
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<{ success: boolean; data: any[] }>
+  res: NextApiResponse<Data>
 ) => {
   // Run the middleware to allow CORS
   await Cors(req, res, {
@@ -16,15 +18,17 @@ const handler = async (
     optionsSuccessStatus: 200,
   });
 
-  const client = await connectToDatabase();
+  res.status(200).json({ name: 'John Doe' });
 
-  if (req.method === "GET") {
-    const shoppingListCollection = client.db().collection("shopping-list");
-    const result = await shoppingListCollection
-      .find({ isDone: false })
-      .toArray();
-    res.status(200).json({ success: true, data: result });
-  }
+//   const client = await connectToDatabase();
+
+//   if (req.method === "GET") {
+//     const shoppingListCollection = client.db().collection("shopping-list");
+//     const result = await shoppingListCollection
+//       .find({ isDone: false })
+//       .toArray();
+//     res.status(200).json({ success: true, data: result });
+//   }
 };
 
 export default handler;
